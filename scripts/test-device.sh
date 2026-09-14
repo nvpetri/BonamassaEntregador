3bf82dac911890237301ceacec6d2f331566a8ad
Nicolas Petri <123608034+nvpetri@users.noreply.github.com>
2026-09-13 21:05:59 -0300

    test: usar API local isolada no emulador

--- scripts/test-device.sh
+++ scripts/test-device.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set +e
-bash gradlew :app:connectedDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.bonamassaIntegration=true --stacktrace
+bash gradlew :app:connectedDebugAndroidTest \
+  -PbonamassaApiUrl=http://10.0.2.2:3001 \
+  -PbonamassaStoreSlug=bonamassa \
+  -Pandroid.testInstrumentationRunnerArguments.bonamassaIntegration=true \
+  --stacktrace
 test_exit=$?
 mkdir -p device-screenshots
 adb pull /data/local/tmp/bonamassa-screenshots/. device-screenshots/ || true
